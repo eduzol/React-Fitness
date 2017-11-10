bd39eeae70319fca45f38e1d6268ff345c4d36e8
EduardoZola <eduzol@gmail.com>
2017-11-10 02:24:32 -0800

    add already logged in state

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -4,6 +4,8 @@ import { getMetricMetaInfo, timeToString } from '../utils/helpers'
 import UdaciSlider from './UdaciSlider';
 import UdaciSteppers from './UdaciSteppers';
 import DateHeader from './DateHeader';
+import { Ionicons } from '@expo/vector-icons';
+import TextButton from './TextButton';
 
 function SubmitBtn ({ onPress }) {
   return (
@@ -55,6 +57,18 @@ export default class AddEntry extends Component {
         }))
     }
 
+    
+    reset = () => {
+    
+        const key = timeToString();
+        
+            // Update Redux
+        
+            // Route to Home
+        
+            // Update "DB"
+    }
+
     submit = () => {
         const key = timeToString()
         const entry = this.state
@@ -67,6 +81,21 @@ export default class AddEntry extends Component {
 
     render() {
         const metaInfo = getMetricMetaInfo()
+
+        if (this.props.alreadyLogged) {
+            return (
+              <View>
+                <Ionicons
+                  name={'ios-happy-outline'}
+                  size={100}
+                />
+                <Text>You already logged your information for today.</Text>
+                <TextButton onPress={this.reset}>
+                  Reset
+                </TextButton>
+              </View>
+            )
+        }
 
         return (
         <View>
