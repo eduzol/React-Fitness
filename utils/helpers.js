3be26b63c649cacf84476c0e5b8fae562a516214
EduardoZola <eduzol@gmail.com>
2017-11-11 22:03:52 -0800

    Adding redux to AddEntry

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,7 +1,13 @@
-import React from 'react'
-import { View } from 'react-native'
-import { FontAwesome, MaterialIcons, MaterialCommunityIcons } from '@expo/vector-icons'
-import {  black } from './colors'
+import React from 'react';
+import { View } from 'react-native';
+import { FontAwesome, MaterialIcons, MaterialCommunityIcons } from '@expo/vector-icons';
+import {  black } from './colors';
+
+export function getDailyReminderValue () {
+  return {
+    today: "👋 Don't forget to log your data today!"
+  }
+}
 
 export function getMetricMetaInfo (metric) {
   const info = {
