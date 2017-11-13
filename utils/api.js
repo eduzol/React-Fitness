8034479ae7034d41af6ffeb7190f204021833bd0
EduardoZola <eduzol@gmail.com>
2017-11-13 01:43:25 -0800

    History component

--- utils/api.js
+++ utils/api.js
@@ -1,11 +1,16 @@
-import { AsyncStorage } from 'react-native'
-import { CALENDAR_STORAGE_KEY } from './_calendar'
+import { AsyncStorage } from 'react-native';
+import { formatCalendarResults, CALENDAR_STORAGE_KEY } from './_calendar';
+
+export function fetchCalendarResults () {
+  return AsyncStorage.getItem(CALENDAR_STORAGE_KEY)
+    .then(formatCalendarResults)
+};
 
 export function submitEntry ({ entry, key }) {
   return AsyncStorage.mergeItem(CALENDAR_STORAGE_KEY, JSON.stringify({
     [key]: entry
   }))
-}
+};
 
 export function removeEntry (key) {
   return AsyncStorage.getItem(CALENDAR_STORAGE_KEY)
@@ -15,4 +20,5 @@ export function removeEntry (key) {
       delete data[key]
       AsyncStorage.setItem(CALENDAR_STORAGE_KEY, JSON.stringify(data))
     })
-}
+};
+
